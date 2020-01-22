d6f40deebc264c1666c4bc7c9ab6c54c461d9738
Joshua Hunt <jelijahhunt@gmail.com>
2020-01-22 23:20:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,3 +21,9 @@ b = Math.floor(Math.random() * 1000)
 function divide(a, b) {
   return(a/b)
 }
+
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+function inc(n) {
+  return(a+1)
+}
