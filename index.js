c237592364322b8484435c5c0471bfffda5b070a
Joshua Hunt <jelijahhunt@gmail.com>
2020-01-22 23:16:45 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+function add(a, b) {
+  return(a+b)
+}
+
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+function subtract(a, b) {
+  return(a-b)
+}
+
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+function multiply(a, b) {
+  return(a*b)
+}
+
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+function divide(a, b) {
+  return(a/b)
+}
