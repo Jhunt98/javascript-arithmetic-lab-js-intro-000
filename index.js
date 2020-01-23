5eb954a0831cc39e05e8cf3319ad90ba20b42fa1
Joshua Hunt <jelijahhunt@gmail.com>
2020-01-23 02:56:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -37,5 +37,9 @@ function dec(n) {
 a = Math.floor(Math.random() * 1000)
 b = Math.floor(Math.random() * 1000)
 function makeInt(n) {
-  return parseInt(n, 10)
+  return parseInt(n, 10)  
+}
+
+function preserveDecimal(n) {
+  return(parseFloat(n, 10))
 }
